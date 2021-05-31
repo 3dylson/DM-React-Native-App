0df475682a240402a617473d6bc7d94ab06e65fc
Luis Gonçalves <zitoluis99@outlook.com>
2021-06-01 00:32:23 +0100

    minor changes

--- screens/index.jsx
+++ screens/index.jsx
@@ -3,3 +3,5 @@ export { default as LoginScreen } from './LoginScreen'
 export { default as HomeScreen } from './HomeScreen'
 
 export { default as SignInScreen } from './SignInScreen'
+
+export { default as PassEditProfile } from './PassEditProfile'
