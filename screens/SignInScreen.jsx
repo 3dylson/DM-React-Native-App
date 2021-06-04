a1bfd0bb328d0a34c4e37092d08488e4e073f315
Luis Gonçalves <zitoluis99@outlook.com>
2021-06-04 01:02:45 +0100

    Password verivication fix

--- screens/SignInScreen.jsx
+++ screens/SignInScreen.jsx
@@ -17,45 +17,57 @@ export default function RegistrationScreen({navigation}) {
         navigation.navigate('Login')
     }
 
-    const onRegisterPress = () => {
-        if (password !== confirmPassword) {
-            alert("Passwords don't match.")
-            return
-        }
-        firebase
-            .auth()
-            .createUserWithEmailAndPassword(email, password)
-            .then((resp) => {
-                if(resp.additionalUserInfo.isNewUser) alert('New user');
-                const uid = resp.user.uid
-                const data = {
-                    id: uid,
-                    contact,
-                   // company,
-                    email,
-                    fullName,
-                };
-                const usersRef = firebase.firestore().collection('users')
-                usersRef
-                    .doc(uid)   // Gets the document reference associated to uid
-                    .set(data)  // Add/Create data to the document reference
-                    .then(() => {   // Set return a promise without parameter's
-                        navigation.navigate('App', {user: data})
-                    })
-                    .catch((error) => {
-                        alert(error)
-                    });
-            })
-            .catch((error) => {
-                let errorCode =  error.code;
-                let errorMessage = error.message;
-                // https://firebase.google.com/docs/reference/js/firebase.auth.Auth#createuserwithemailandpassword
-                if(error.code=='auth/operation-not-allowed') alert('Registry with email and password is not enabled');
-                if(error.code=='auth/weak-password') alert('Password is too weak');
-                if(error.code=='auth/invalid-email') alert('Invalid email');
-                if(error.code=='auth/email-already-in-use') alert('Email already in use');
-                
-        });
+    const onRegisterPress = () =>{
+    //if (password !== confirmPassword) {
+           // alert("Passwords don't match.")
+           // return
+        //}
+
+        if(password.trim()&&confirmPassword.trim()){
+            //try {
+                if(password == confirmPassword) {
+
+                    firebase
+                        .auth()
+                        .createUserWithEmailAndPassword(email, password)
+                        .then((resp) => {
+                            if(resp.additionalUserInfo.isNewUser) alert('New user');
+                            const uid = resp.user.uid
+                            const data = {
+                                id: uid,
+                                contact,
+                               // company,
+                                email,
+                                fullName,
+                            };
+                            const usersRef = firebase.firestore().collection('users')
+                            usersRef
+                                .doc(uid)   // Gets the document reference associated to uid
+                                .set(data)  // Add/Create data to the document reference
+                                .then(() => {   // Set return a promise without parameter's
+                                    navigation.navigate('App', {user: data})
+                                })
+                                .catch((error) => {
+                                    alert(error)
+                                });
+                        })
+                        .catch((error) => {
+                            let errorCode =  error.code;
+                            let errorMessage = error.message;
+                            // https://firebase.google.com/docs/reference/js/firebase.auth.Auth#createuserwithemailandpassword
+                            if(error.code=='auth/operation-not-allowed') alert('Registry with email and password is not enabled');
+                            if(error.code=='auth/weak-password') alert('Password is too weak');
+                            if(error.code=='auth/invalid-email') alert('Invalid email');
+                            if(error.code=='auth/email-already-in-use') alert('Email already in use');
+                            
+                    });}
+                else {
+                    alert('Passwords dont match');
+                }
+                    
+            } else if (password.trim()&& !confirmPassword.trim()){
+                    alert('Must confirm new password')
+                }
     }
 
     return (
