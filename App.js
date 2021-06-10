c41784a1c2d326c20e6c82747fa19a5d8b567906
Edylson Manuel Gonçalves Frederico <edylson_frederico@outlook.com>
2021-06-10 03:57:23 +0100

    init firebase, redux

--- App.js
+++ App.js
@@ -1,34 +1,77 @@
 import 'react-native-gesture-handler';
-import React, { Component } from 'react'
-import {StatusBar} from "react-native"
-import { NavigationContainer } from '@react-navigation/native'
-import {decode, encode} from 'base-64'
-import AuthStackScreen from './navigation/BottomTabNavigator';
+import React, { Component } from 'react';
+import {firebase} from './firebase/config';
+import {View, Text} from 'react-native';
+import Loading from './animations/Loading';
+import {StatusBar} from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import {decode, encode} from 'base-64';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { AuthContext } from './firebase/context';
 
- if (!global.btoa) {  global.btoa = encode }
- if (!global.atob) { global.atob = decode }
+import LandingScreen from './components/auth/Landing'
+import RegisterScreen from './components/auth/Register'
+
+if (!global.btoa) {  global.btoa = encode }
+if (!global.atob) { global.atob = decode }
  
+
+const Stack = createStackNavigator();
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      loaded: false,
+    }
+  }
+
+  componentDidMount(){
+    firebase.auth().onAuthStateChanged((user) => {
+      if(!user){
+        this.setState({
+          loggedIn: false,
+          loaded: true,
+        })
+      }else {
+        this.setState({
+          loggedIn: true,
+          loaded: true,
+        })
+      }
+    })
   }
 
   render() {
-    return (
-      <SafeAreaProvider>
-        <NavigationContainer style={{flex: 1}}>
-          { AuthStackScreen(this.context.isLogged)}
-        </NavigationContainer>
-        <StatusBar/>
-      </SafeAreaProvider>
+    const {loggedIn, loaded} = this.state;
+    if(!loaded){
+      return(
+        <Loading style={{ flex: 1, justifyContent: 'center'}}/>
+      )
+    }
+    if(!loggedIn){
+      return (
+        <SafeAreaProvider>
+          <NavigationContainer>
+            <Stack.Navigator initialRouteName="Landing">
+              <Stack.Screen name="Landing" component={LandingScreen} options={{ headerShown: false}}/>
+              <Stack.Screen name="Register" component={RegisterScreen}/>
+            </Stack.Navigator>
+          </NavigationContainer>
+          <StatusBar/>
+        </SafeAreaProvider>
+      );
+    }
+    return(
+      <View>
+        <Text>
+          User is logged!
+        </Text>
+      </View>
     )
   }
 }
 
-App.contextType = AuthContext;
+
 
  
---
+++ components/auth/Register.jsx
@@ -0,0 +1,153 @@
+import React, { Component } from 'react'
+import { View, Button, TextInput, TouchableOpacity, Image } from 'react-native'
+import styles from '../../styles/signInStyle'
+import logo from '../../assets/logo.png'
+import {Picker} from '@react-native-picker/picker';
+import {firebase} from '../../firebase/config';
+
+export class Register extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            id: '',
+            fullName: '',
+            contact: '', 
+            email: '',
+            company: '',
+            password: '',
+            confirmPassword: ''
+        }
+
+        this.onRegisterPress = this.onRegisterPress.bind(this)
+
+    }
+
+    onRegisterPress(){
+        const {fullName, contact, email, company, password, confirmPassword} = this.state;
+        if (password.trim() && !confirmPassword.trim()) {
+            alert("Must confirm password")
+            return
+        }
+        else if(password !== confirmPassword){
+            alert("Passwords don't match.")
+            return
+        }
+
+        firebase.auth().createUserWithEmailAndPassword(email,password)
+        .then((resp) => {
+            if(resp.additionalUserInfo.isNewUser) alert('New User created!');
+            const uid = resp.user.uid
+            const data = {
+                id: uid,
+                fullName,
+                contact,
+                email,
+                company,
+            };
+            const usersRef = firebase.firestore().collection('users')
+            usersRef.doc(uid).set(data)
+            .then(() => {
+                // Navigate
+            })
+            .catch((error) => {
+                alert(error)
+            });
+        })
+        .catch((error) => {
+            let errorCode =  error.code;
+            let errorMessage = error.message;
+            // https://firebase.google.com/docs/reference/js/firebase.auth.Auth#createuserwithemailandpassword
+            if(error.code=='auth/operation-not-allowed') alert('Registry with email and password is not enabled');
+            if(error.code=='auth/weak-password') alert('Password is too weak');
+            if(error.code=='auth/invalid-email') alert('Invalid email');
+            if(error.code=='auth/email-already-in-use') alert('Email already in use');            
+    });
+
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <KeyboardAwareScrollView
+                    style={{ flex: 1, width: '100%' }}
+                    keyboardShouldPersistTaps="always">
+                    <Image
+                        style={styles.logo}
+                        source={require(logo)}
+                    />
+                    <TextInput
+                        style={styles.input}
+                        placeholder='Full Name'
+                        placeholderTextColor="#aaaaaa"
+                        onChangeText={(text) => this.setState({fullName: text})}
+                        value={this.state.fullName}
+                        underlineColorAndroid="transparent"
+                        autoCapitalize="none"
+                    />
+                    <TextInput
+                        style={styles.input}
+                        placeholder='Contact'
+                        placeholderTextColor="#aaaaaa"
+                        onChangeText={(text) => this.setState({contact: text})}
+                        value={this.state.contact}
+                        underlineColorAndroid="transparent"
+                        autoCapitalize="none"
+                    />
+
+                    <Picker
+                        style={styles.input}
+                        placeholder='Company'
+                        placeholderTextColor="#aaaaaa"
+                        selectedValue={this.state.company}
+                        onValueChange={(itemValue, itemIndex) =>
+                            this.setState({company: itemValue})
+                        }>
+                            <Picker.Item label="IPB" value="ipb"/>
+                            <Picker.Item label="TheMarket" value="tmarket"/>
+                        </Picker>
+                
+                    <TextInput
+                        style={styles.input}
+                        placeholder='E-mail'
+                        placeholderTextColor="#aaaaaa"
+                        onChangeText={(text) => this.setState({email:text})}
+                        value={this.state.email}
+                        underlineColorAndroid="transparent"
+                        autoCapitalize="none"
+                    />
+                    <TextInput
+                        style={styles.input}
+                        placeholderTextColor="#aaaaaa"
+                        secureTextEntry
+                        placeholder='Password'
+                        onChangeText={(text) => this.setState({password: text})}
+                        value={this.state.password}
+                        underlineColorAndroid="transparent"
+                        autoCapitalize="none"
+                    />
+                    <TextInput
+                        style={styles.input}
+                        placeholderTextColor="#aaaaaa"
+                        secureTextEntry
+                        placeholder='Confirm Password'
+                        onChangeText={(text) => this.setState({confirmPassword: text})}
+                        value={this.state.confirmPassword}
+                        underlineColorAndroid="transparent"
+                        autoCapitalize="none"
+                    />
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => this.onRegisterPress()}>
+                        <Text style={styles.buttonTitle}>Create account</Text>
+                    </TouchableOpacity>
+                    <View style={styles.footerView}>
+                        <Text style={styles.footerText}>Already got an account? <Text onPress={this.onFooterLinkPress} style={styles.footerLink}>Log in</Text></Text>
+                    </View>
+                </KeyboardAwareScrollView>
+            </View>
+        )
+    }
+}
+
+export default Register
