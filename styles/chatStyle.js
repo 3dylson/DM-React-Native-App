60fcb81dba496656d301ad16148b4344e38d3ade
Luis Ribeiro <luiscarlos1184.lr@gmail.com>
2021-06-10 23:16:24 +0100

    Chat

---
+++ styles/chatStyle.js
@@ -0,0 +1,51 @@
+/*import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create(
+    {
+        Container: {
+            flex: 1,
+            alignItems: 'center'
+        },
+        logo: {
+            flex: 1,
+            height: 120,
+            width: 90,
+            alignSelf: "center",
+            margin: 30
+        },
+        majorCont: {
+            flexDirection: 'column',
+            height: 50,
+            backgroundColor: 'white'
+        },
+        chatName: {
+            fontSize: 20,
+            color: '#2e2e2d'
+        },
+        button: {
+            backgroundColor: '#788eec',
+
+            marginRight: 5,
+            marginTop: 40,
+            height: 48,
+            borderRadius: 5,
+            alignItems: "center",
+            justifyContent: 'center'
+        },
+        buttonText: {
+            color: 'white',
+            fontSize: 16
+        },
+
+        caixaMsg:{
+            backgroundColor: 'white',
+            height: 50,
+            alignSelf: 'center',
+            marginRight: 10,
+            marginLeft: 10,
+            
+        }
+
+
+    }
+) **/
