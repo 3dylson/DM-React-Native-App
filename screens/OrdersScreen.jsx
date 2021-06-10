22f80985f3d420938bfad14d978d73a7db36a924
Edylson Manuel Gonçalves Frederico <edylson_frederico@outlook.com>
2021-06-10 22:10:12 +0100

    order screen

--- screens/OrdersScreen.jsx
+++ screens/OrdersScreen.jsx
@@ -1,166 +1,62 @@
-import React, { Component, useEffect, useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { FlatList, Keyboard, Text, TextInput, TouchableOpacity, View, Button } from 'react-native'
 import styles from '../styles/homeStyle';
 import { firebase } from '../firebase/config'
+import { connect } from 'react-redux'
 
-export default class OrdersScreen extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            entities: '',
-            entityText:'',
-            entityToUpdate: ''
-        }
-    }
+function OrdersScreen(props) {
 
-    // checkParams = () => {
-    //     var params = this.props.navigation.state.params;
-    //     if (params.userId) {
-    //         this.setState({ userId: params.userId })
-    //     }
-    //     this.fetchUserInfo(params.userId)
-    // }
+    const[orders, setOrders] = useState([]);
 
-    // const [entityText, setEntityText] = useState('');
-    // const [entities, setEntities] = useState([]);
-    // const [entityToUpdate, setEntityToUpdate] = useState(null);
-
-    //const entityRef = firebase.firestore().collection('entities')
-    //const userID = props.extraData.id
-
-    // useEffect(() => {
-    //     entityRef
-    //         .where("authorID", "==", userID)
-    //         .orderBy('createdAt', 'desc')
-    //         .onSnapshot(
-    //             querySnapshot => {
-    //                 const newEntities = []
-    //                 querySnapshot.forEach(doc => {                        
-    //                     const entity = doc.data() // Entity record, try alert(entity.text)
-    //                     entity.id = doc.id
-    //                     newEntities.push(entity)    // Update the state
-    //                 });
-    //                 setEntities(newEntities)
-    //             },
-    //             error => {
-    //                 console.log(error)
-    //             }
-    //         )
-    // }, [])
-
-    onAddButtonPress = () => {
-        if (this.state.entityText && this.state.entityText.length > 0) {
-            if(!entityToUpdate) {
-                // FieldValue - Sentinel values that can be used when writing document fields with set() or update().
-                // serverTimestamp() - Returns a sentinel used with set() or update() to include a server-generated timestamp in the written data.
-                const timestamp = firebase.firestore.FieldValue.serverTimestamp();
-                const data = {
-                    text: this.state.entityText,
-                    authorID: userID,
-                    createdAt: timestamp,
-                };
-                entityRef
-                    .add(data)
-                    .then(_doc => {
-                        this.setState({entityText: ''})
-                        Keyboard.dismiss()
-                    })
-                    .catch((error) => {
-                        alert(error)
-                    });
-            }
-            else {
-                const {id, authorID} = entityToUpdate;
-                entityRef
-                    .doc(id)
-                    .update({ ...entityToUpdate, text: entityText })
-                    .then(() => {
-                        alert(id + "updated with success");
-                        this.setState({entityText: ''})
-                        this.setState({entityToUpdate: null})
-                    })
-                    .catch((error) => {
-                        alert(error);
-                    });
-            }
-        }
-    }
-
-    onDeleteButtonPress = (entity) => {        
-        const { id } = entity;
-        entityRef
-            .doc(id)
-            .delete()
-            .then(() => {
-                alert(id + "deleted with success");
+    useEffect(() => {
+        
+            props.feed.sort(function(x,y){
+                return x.creation - y.creation;
             })
-            .catch((error) => {
-                alert(error);
-            });
-    }
-
-    renderEntity = ({item, index}) => {
-        return (
-            <View style={styles.entityContainer}>
-                <Text style={styles.entityText} onPress={() => {
-                    this.setState({entityText: item.text})
-                    this.setState({entityToUpdate: item})
-                }}>
-                        {index}. {item.text}        
-                </Text>                
-                <Button color="red" onPress={() => { onDeleteButtonPress(item) }} title={"X"}/>
-            </View>
-        )
-    }
+            setOrders(props.feed);
+        
+        
+    }, [props.feed])
 
 
-    cancelUpdate = () => {
-        this.setState({EntityToUpdate: null}),
-        this.setState({entityText: ''})
-    }
+    const onDetailsPress = (userId, orderId) => {
+        // Ir ao servidor..
+        // Navegar para vista detalhada da encomenda.. 
 
-     logout = () => {
-        firebase.auth().signOut();
-    }
+    } 
 
-    render() {
-    return (
+    return(
         <View style={styles.container}>
-            <View style={styles.button}>
-                <Button
-                title="Logout Test"
-                onPress={() => this.logout()}/>
-            {/* {entityToUpdate && 
-                    <TouchableOpacity style={[styles.buttonCancel, { marginRight:10 }]} onPress={() => cancelUpdate()}>
-                        <Text style={styles.buttonText}>Cancel</Text>
-                    </TouchableOpacity>
-                 }
-                <TextInput
-                    style={styles.input}
-                    placeholder='Add new entity'
-                    placeholderTextColor="#aaaaaa"
-                    onChangeText={(text) => setEntityText(text)}
-                    value={entityText}
-                    underlineColorAndroid="transparent"
-                    autoCapitalize="none"
+            <View style={styles.containerGallery}>
+                <FlatList
+                    numColumns={1}
+                    horizontal={false}
+                    data={orders}
+                    renderItem={({ item }) => (
+                        <View
+                            >
+                            <Text >{item.id}</Text>
+                            
+                            <Text
+                                onPress={() => props.navigation.navigate('OrderDetails', { orderId: item.id, uid: item.user.uid })}>
+                                View Details...
+                                </Text>
+                        </View>
+
+                    )}
+
                 />
-                <TouchableOpacity style={styles.button} onPress={onAddButtonPress}>
-                    <Text style={styles.buttonText}>
-                        { entityToUpdate ? "Update" : "Add" }
-                    </Text>
-                </TouchableOpacity>                
             </View>
-            { entities && (
-                <View style={styles.listContainer}>
-                    <FlatList
-                        data={entities}
-                        renderItem={renderEntity}
-                        keyExtractor={(item) => item.id}
-                        removeClippedSubviews={true}
-                    />
-                </View>
-            )} */}
         </View>
-        </View>
+
     )
-}}
+    
+}
+
+const mapStateToProps = (store) => ({
+    currentUser: store.userState.currentUser,
+    feed: store.usersState.feed,
+})
+
+export default connect(mapStateToProps,null)(OrdersScreen);
+
